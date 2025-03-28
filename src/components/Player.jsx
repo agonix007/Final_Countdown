0b0b8dd4169e92c7006ca911434c4843f5f889d9
Bikram <technology2203@gmail.com>
2025-03-28 22:58:03 +0530

    Add player name input functionality to Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleNameChange = (event) => {
+    setSubmitted(false);   
+    setEnteredPlayerName(event.target.value);
+  };
+
+  const handleClick = () => {
+    setSubmitted(true);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          type="text"
+          onChange={handleNameChange}
+          value={enteredPlayerName}
+        />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
