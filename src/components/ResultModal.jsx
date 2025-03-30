d26f23be1f0f08228426895b566c0cb68fa24bd8
Bikram <technology2203@gmail.com>
2025-03-30 22:19:31 +0530

    Refactor ResultModal to use createPortal for rendering and improve modal structure

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,22 +1,27 @@
 import { useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
-export default function ResultModal({ ref, targetTime, remainingTime, onReset }) {
+export default function ResultModal({
+  ref,
+  targetTime,
+  remainingTime,
+  onReset,
+}) {
   const dialog = useRef();
 
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
   const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
-
   useImperativeHandle(ref, () => {
     return {
       open() {
         dialog.current.showModal();
-      }
-    }
+      },
+    };
   });
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You lost</h2>}
       {!userLost && <h2>You Score: {score}</h2>}
@@ -27,8 +32,9 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
         You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
       <form method="dialog" onSubmit={onReset}>
-        <button>CLose</button>
+        <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
-}
+} 
